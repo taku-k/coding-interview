c84652d500272c7abcc0f013706da205379587c1
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-03 01:10:53 +0900

    Add more practices for leercode

---
+++ src/main/java/leetcode/PermutationKthSequence.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PermutationKthSequence {
+    public String getPermutation(int n, int k) {
+        List<Integer> num = new ArrayList<>();
+        for (int i = 1; i <= n; ++i) {
+            num.add(i);
+        }
+        int[] fact = new int[n];
+        fact[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            fact[i] = i * fact[i - 1];
+        }
+        k--;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n - 1; ++i) {
+            sb.append(num.get(k / fact[n - 1 - i]));
+            num.remove(k / fact[n - 1 - i]);
+            k %= fact[n - 1 - i];
+        }
+        return sb.toString();
+    }
+}
