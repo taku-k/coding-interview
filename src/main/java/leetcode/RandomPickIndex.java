0b6a2acc5a650332122fc690fd29c659a5f6825c
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-04 09:49:23 +0900

    Add more practices for leetcode

---
+++ src/main/java/leetcode/RandomPickIndex.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.*;
+
+public class RandomPickIndex {
+    private Map<Integer, List<Integer>> indicesOf;
+    private Random gen;
+
+    public RandomPickIndex(int[] nums) {
+        indicesOf = new HashMap<>();
+        gen = new Random();
+        for (int i = 0; i < nums.length; ++i) {
+            indicesOf.putIfAbsent(nums[i], new ArrayList<>());
+            indicesOf.get(nums[i]).add(i);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> indices = indicesOf.get(target);
+        return indices.get(gen.nextInt(indices.size()));
+    }
+}
